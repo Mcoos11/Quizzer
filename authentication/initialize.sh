9d2b90aa7586b7cbceaf43d97f9d9b04b3107619
Mcoos11 <marcinc121@gmail.com>
2023-12-17 17:09:45 +0100

    modified:   .gitignore 	modified:   authentication/initialize.sh 	deleted:    data-auth/base/1/pg_internal.init 	deleted:    data-auth/global/pg_internal.init 	deleted:    data-auth/postmaster.pid 	deleted:    data-quiz/base/1/pg_internal.init 	deleted:    data-quiz/global/pg_internal.init 	deleted:    data-quiz/postmaster.pid

--- authentication/initialize.sh
+++ authentication/initialize.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-python manage.py makemigrations
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+python manage.py migrate
+python manage.py collectstatic
 
 gunicorn authentication.wsgi:application --bind 0.0.0.0:8000
