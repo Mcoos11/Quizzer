4a9cb95b214f7966c44acabf42dd6884d43463ec
Mcoos11 <marcinc121@gmail.com>
2023-12-17 18:19:25 +0100

    modified:   authentication/authentication/settings.py 	modified:   authentication/initialize.sh 	modified:   docker-compose.yml 	modified:   frontend/.env 	modified:   nginx/nginx-setup.conf

--- authentication/initialize.sh
+++ authentication/initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python manage.py migrate
-python manage.py collectstatic
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 
 gunicorn authentication.wsgi:application --bind 0.0.0.0:8000
