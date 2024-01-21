0167ce566a0381e0416a0c927fe3df8def781a34
Mcoos11 <marcinc121@gmail.com>
2024-01-21 17:57:59 +0100

    hacks

--- surveymaker/initialize.sh
+++ surveymaker/initialize.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn quizmaker.wsgi:application --bind 0.0.0.0:8000
+gunicorn surveymaker.wsgi:application --bind 0.0.0.0:8000
